acf7bc554558ed7cbff71114e06c3fc2604a487c
Neeraj <neeraj@NeerajtekiMacBook-Pro.local>
2022-12-09 19:00:59 +0530

    update code

--- blocks/columns/doc-element.js
+++ blocks/columns/doc-element.js
@@ -1,6 +1,6 @@
 [
   ['  Columns '],
   ['https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/columns/columns1.png', ' Column Block 1 '],
-  ['columns2', 'https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/columns/columns2.jpeg']
+  ['columns2', 'https://raw.githubusercontent.com/neerajgrg/MyHelixProject/main/blocks/columns/columns2.png']
 ];
 
