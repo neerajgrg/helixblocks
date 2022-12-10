908f17bc5caabae30e09b5440b0537fb3a12237e
Neeraj <neeraj@NeerajtekiMacBook-Pro.local>
2022-12-10 11:00:59 +0530

    updated files

---
+++ blocks/winners-carousel/doc-table.js
@@ -0,0 +1,8 @@
+[
+    ['  Winners Carousel '],
+    ['https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/winners-carousel/neeraj.jpeg', ' Winner1 ', ' Neeraj Garg ', ' Cool '],
+    ['https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/winners-carousel/abhishek.jpeg', ' Winner2 ', ' Abhisek Dwevedi ', ' Cool '],
+    ['https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/winners-carousel/musk.jpeg', ' Winner3 ', ' Elon Musk ', ' Twitter Owner :)'],
+  ];
+  
+  
