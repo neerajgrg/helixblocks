1319d97c83fedccffed1536254809de21f0ee47b
neeraj <neerajgrg93@gmail.com>
2022-12-11 00:20:19 +0530

    helix blocks updated

--- blocks/hero/doc-table.js
+++ blocks/hero/doc-table.js
@@ -1,9 +1,6 @@
- var body = DocumentApp.getActiveDocument().getBody();
-      // Create a two-dimensional array containing the cell contents.
-  var cells = [
-    ['  HERO '],
-    ['https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/cards/card1.jpeg']
-  ];
-
-  // Build a table from the array.
-  body.appendTable(cells);
+[
+  ["  HERO "],
+  [
+    "https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/cards/card1.jpeg"
+  ],
+]
