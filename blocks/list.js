e60d7ebc43cc82587de1584c42bac0cd55bf5498
neerajgrg <neerajgrg93@gmail.com>
2022-12-10 18:06:31 +0530

    Create list.js

---
+++ blocks/list.js
@@ -0,0 +1,15 @@
+const blocks = [
+    'cards',
+    'columns',
+    'embed',
+    'footer',
+    'form',
+    'header',
+    'hero',
+    'metadata',
+    'poll',
+    'section-background',
+    'winners-carousel'
+  ];
+  
+  export default blocks;
