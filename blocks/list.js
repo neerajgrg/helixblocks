815bd94d7253140e10ae8f6e5a3a3aecfc884433
Neeraj <neeraj@NeerajtekiMacBook-Pro.local>
2022-12-10 18:09:45 +0530

    block list added

--- blocks/list.js
+++ blocks/list.js
@@ -1,15 +1,37 @@
 const blocks = [
-    'cards',
-    'columns',
-    'embed',
-    'footer',
-    'form',
-    'header',
-    'hero',
-    'metadata',
-    'poll',
-    'section-background',
-    'winners-carousel'
-  ];
+    {
+      'name': 'cards'
+    },
+    {
+      'name': 'columns'
+    },
+    {
+      'name': 'embed'
+    },
+    {
+      'name': 'footer'
+    },
+    {
+      'name': 'form'
+    },
+    {
+      'name': 'header'
+    },
+    {
+      'name': 'hero'
+    },
+    {
+      'name': 'metadata'
+    },
+    {
+      'name': 'poll'
+    },
+    {
+      'name': 'section-background'
+    },
+    {
+      'name': 'winners-carousel'
+    }
+];
   
   export default blocks;
