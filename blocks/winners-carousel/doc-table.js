ed649cbe4592e4c5eb3b83f62b7a298f314b7dc2
neeraj <neerajgrg93@gmail.com>
2022-12-12 19:05:13 +0530

    updated entry

--- blocks/winners-carousel/doc-table.js
+++ blocks/winners-carousel/doc-table.js
@@ -2,7 +2,7 @@
     ['  Winners Carousel '],
     ['https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/winners-carousel/winner.jpeg', ' Winner1 ', ' MR. GARG ', ' Idea1 '],
     ['https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/winners-carousel/first.png', ' Winner2 ', ' MR.Dwivedi ', ' Idea2 '],
-    ['https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/winners-carousel/second.png', ' Winner3 ', ' MR. Sandeep ', '  Idea3 '],
+    ['https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/winners-carousel/second.jpeg', ' Winner3 ', ' MR. Sandeep ', '  Idea3 '],
   ];
   
   
