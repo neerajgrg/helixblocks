e420fc710cb0d408fa49da32b5d004dedbbba55d
neeraj <neerajgrg93@gmail.com>
2022-12-13 20:21:40 +0530

    cards added

---
+++ blocks/cards/doc-table.js
@@ -0,0 +1,15 @@
+[
+  ["  Cards "],
+  [
+    "https://raw.githubusercontent.com/neerajgrg/HelloFranklin/main/blocks/cards/card1.jpeg",
+    " Helix is the fastest way to publish, create, and serve websites ",
+  ],
+  [
+    "https://raw.githubusercontent.com/neerajgrg/HelloFranklin/main/blocks/cards/card2.jpeg",
+    " Helix allows you to publish more content in shorter time with smaller teams",
+  ],
+  [
+    "https://raw.githubusercontent.com/neerajgrg/HelloFranklin/main/blocks/cards/card3.jpeg",
+    " Preview content at 100% fidelity, get predictable content velocity, and shorten project durations",
+  ],
+];
