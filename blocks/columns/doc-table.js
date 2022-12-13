47744d3ae602811e083b8a3e582139dc42c4c4e5
neeraj <neerajgrg93@gmail.com>
2022-12-13 20:29:01 +0530

    doc table updated

--- blocks/columns/doc-table.js
+++ blocks/columns/doc-table.js
@@ -1,7 +1,7 @@
 [
   ["  Columns "],
   [
-    "Columns Text",
+    "Adobe Experience Manager (AEM) is a comprehensive content management solution that makes it easy to manage your marketing content and assets.",
     "https://raw.githubusercontent.com/neerajgrg/helixblocks/main/blocks/columns/column1.png",
-  ]
+  ],
 ];
